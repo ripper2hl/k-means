e5b5f118d6e6e105cbf86c04ec034bac0e1980af
Jesus Perales <risa-beat-box@hotmail.com>
2013-11-03 13:12:01 -0600

    inicio k-means

---
+++ public/javascripts/KMeans.js
@@ -0,0 +1,113 @@
+/**
+*Funcion que recorre los puntos y muestra su informacion
+*/
+function recorrerPuntos(desde,hasta){
+    for(i = desde-1; i < hasta;i++){
+        console.log("Punto " + (i+1) , "x = " + puntos[i].x, ", y = " + puntos[i].y);
+    }
+    return "Se recorrio desde el punto: "+ desde + ", hasta el punto: " + hasta;
+}
+
+/**
+*Funcion que retorna la distancia euclidiana entre un punto dado y su centroide
+*es la interpretacion de la siguiente formula
+*distancia(p,c)= raiz cuadrada del punto en "x" menos el centroide en "x" al cuadrado
+*mas el punto en y menos el centroide en y al cuadrado  
+*/
+
+function calcularDist(p, c){
+
+    var distancia;
+    var a = Math.pow((p.x-c.x),2) + Math.pow((p.y-c.y),2);
+    distancia = Math.sqrt(a);
+    return distancia;
+}
+
+/*
+ *retorna la distancia euclidiana de los puntos dados al darle 
+ *los centroides requeridos y la almacena en los puntos en las 
+ *variables distEuC1 distEuC2 de cada punto
+ */
+function obtenerDist(puntos,centroides){
+    
+    for(var i = 0;i< DATOS_TOTALES;i++){
+	
+	for(var j = 0;j < centroides.length;j++){
+	    
+	    if(j === 0){
+
+		puntos[i].distEuC1 = calcularDist(puntos[i],centroides[j]);
+	    }else{
+
+		puntos[i].distEuC2 = calcularDist(puntos[i],centroides[j]);
+	    }
+	}
+    }
+    return puntos;
+}
+
+/*
+ *Agrega un nuevo valor a la variable puntos para 
+ *saber a que grupo de centroide pertenece
+ */
+function agrupamiento(puntos){
+    
+    for(i = 0; i < puntos.length;i++){
+	
+	for(j = 0; j < centroides.length; j++){
+	    
+	    //Si la distancia euclidiana al centroide 1 es menor a a la del centroide 2 el punto pertenece al grupo del primer centroide
+	    if(puntos[i].distEuC1 < puntos[i].distEuC2){
+		
+		puntos[i].grupo = 0;
+	    }else{
+		
+		puntos[i].grupo = 1;
+	    }
+
+	}
+    }
+    return puntos;
+}
+
+/*
+ *Recalcula los centroides
+ */
+function recalcularCent(puntos,centroides){
+
+    var sumaPuntosX = [2];
+    var sumaPuntosY = [2];
+    sumaPuntosX[0] = 0;
+    sumaPuntosY[0] = 0;
+    sumaPuntosX[1] = 0;
+    sumaPuntosY[1] = 0;
+    
+    //Contadores para los elementos de cada grupo
+    var elementosG = [2];
+    elementosG[0] = 0;
+    elementosG[1] = 0;
+
+    //Suma de puntos de su valor en  x/ y pertenecientes al grupo del  centroide 1
+    for(i = 0; i < puntos.length; i++){
+	
+	if(puntos[i].grupo == 0){
+
+	    sumaPuntosX[0] += puntos[i].x;
+	    sumaPuntosY[0] += puntos[i].y;
+	    elementosG[0]++;
+	}else{
+	    
+	    sumaPuntosX[1] += puntos[i].x;
+	    sumaPuntosY[1] += puntos[i].y;
+	    elementosG[1]++;
+	}
+    }
+
+    //Cambio de valores a los centroides en sus puntos x/y con el uso de la formula
+    for(i = 0;i < centroides.length;i++){
+
+	centroides[i].x = sumaPuntosX[i]/elementosG[i];
+	centroides[i].y = sumaPuntosY[i]/elementosG[i];
+    }
+        
+}
---
+++ public/javascripts/script.js
@@ -0,0 +1,99 @@
+var puntos = [
+    { "id": 1, "x" : 0, "y" : 0 },
+    { "id": 2, "x" : 0, "y" : 0 },
+    { "id": 3, "x" : 0, "y" : 0 },
+    { "id": 4, "x" : 0, "y" : 0 },
+    { "id": 5, "x" : 0, "y" : 0}
+];
+
+var centroides =  [
+    { "id": 1, "x" : 0, "y" : 0 },
+    { "id": 2, "x" : 0, "y" : 0 }
+];
+
+var DATOS_TOTALES = puntos.length;
+
+function ejecutar(puntos, centroides){
+    
+    //datos
+    var fuente = $('#plantilla-tabla-datos').html();
+    var plantilla = Handlebars.compile(fuente);
+    var html = plantilla(puntos);
+    $('#tabla-datos').html(html);
+    
+    //centroides
+    var fuente = $('#plantilla-tabla-centroides').html();
+    var plantilla = Handlebars.compile(fuente);
+    var html = plantilla(centroides);
+    $('#tabla-centroides').html(html);
+    
+    
+    //Calcular distancia Ecuclidiana
+    obtenerDist(puntos,centroides);
+    var fuente = $('#plantilla-tabla-distancia').html();
+    var plantilla = Handlebars.compile(fuente);
+    var html = plantilla(puntos);
+    $('#tabla-distancia').html(html);
+    
+    //Agrupar los mas cercanos a los Centroides
+    agrupamiento(puntos);
+    var fuente = $('#plantilla-tabla-grupos').html();
+    var plantilla = Handlebars.compile(fuente);
+    var html = plantilla(puntos);
+    $('#tabla-grupos').html(html);
+
+    
+    console.log("datos antes de recalcular");
+    console.log(puntos);
+    console.log(centroides);
+    
+    //Recalcular Centroides
+    recalcularCent(puntos,centroides);
+
+    console.log("datos antes de recalcular");
+    console.log(puntos);
+    console.log(centroides);
+    var fuente = $('#plantilla-tabla-centroides-recalculados').html();
+    var plantilla = Handlebars.compile(fuente);
+    var html = plantilla(centroides);
+    $('#tabla-centroides-recalculados').html(html);
+
+    return centroides;
+}
+
+$(document).ready(function(){
+
+    ejecutar(puntos,centroides);
+    
+    $("#ejecutar").click( function(e) {
+	
+	ejecutar(puntos,centroides);
+
+    });
+
+    $("td").on('change','.puntos',function(e){
+	
+	puntos[0].x = parseInt( $("#1-x").val() );
+	puntos[1].x = parseInt( $("#2-x").val() );
+	puntos[2].x = parseInt( $("#3-x").val() );
+	puntos[3].x = parseInt( $("#4-x").val() );
+	puntos[4].x = parseInt( $("#5-x").val() );
+
+	puntos[0].y = parseInt( $("#1-y").val() );
+	puntos[1].y = parseInt( $("#2-y").val() );
+	puntos[2].y = parseInt( $("#3-y").val() );
+	puntos[3].y = parseInt( $("#4-y").val() );
+	puntos[4].y = parseInt( $("#5-y").val() );
+	
+    });
+
+    $("td").on('change','.centroides',function(e){
+	
+	centroides[0].x = $("#c1-x").val();
+	centroides[1].x = $("#c2-x").val();
+	
+	centroides[0].y = $("#c1-y").val();
+	centroides[1].y = $("#c2-y").val();
+    });
+    
+});
